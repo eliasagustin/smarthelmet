f03e2469f7d548ecba6550d82d6f307ce7612a51
Agustin <eliasagustin@gmail.com>
2023-01-27 20:31:48 -0300

    checking errors on components created

--- smart-helmet/src/app/app.module.ts
+++ smart-helmet/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/productViewer/header/header.component';
-import { HerpSectionComponent } from './components/productViewer/HeroSection/herp-section/herp-section.component';
 import { HeroSectionComponent } from './components/productViewer/hero-section/hero-section.component';
 import { ProductFeaturesComponent } from './components/productViewer/product-features/product-features.component';
 import { TestimonialsComponent } from './components/productViewer/testimonials/testimonials.component';
@@ -14,7 +13,6 @@ import { FooterComponent } from './components/productViewer/footer/footer.compon
   declarations: [
     AppComponent,
     HeaderComponent,
-    HerpSectionComponent,
     HeroSectionComponent,
     ProductFeaturesComponent,
     TestimonialsComponent,
