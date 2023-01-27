599a5b8b8eba097ccd914b0a019d6e020227398a
Agustin <eliasagustin@gmail.com>
2023-01-27 20:23:19 -0300

    basic components idea

--- smart-helmet/src/app/app.module.ts
+++ smart-helmet/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/productViewer/header/header.component';
+import { HerpSectionComponent } from './components/productViewer/HeroSection/herp-section/herp-section.component';
+import { HeroSectionComponent } from './components/productViewer/hero-section/hero-section.component';
+import { ProductFeaturesComponent } from './components/productViewer/product-features/product-features.component';
+import { TestimonialsComponent } from './components/productViewer/testimonials/testimonials.component';
+import { FooterComponent } from './components/productViewer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HerpSectionComponent,
+    HeroSectionComponent,
+    ProductFeaturesComponent,
+    TestimonialsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
